f428720dfa9932ea2f47948e35d0762ef42a8f81
Samarth2902 <samarthathreya29@gmail.com>
2018-01-24 17:54:28 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+web3.eth.defaultAccount = web3.eth.accounts[0];
+abi = JSON.parse()
+postRecord = web3.eth.contract(abi);
+contractInstance = postRecord.at(0xca35b7d915458ef540ade6068dfe2f44e8fa733c);
+import { default as Web3} from 'web3';
+
+window.nombreTop = function() {
+  let label_id = nombreTop;
+  var patientID = (nombreTop($("input.text").val()));
+}
+
+
+window.id = function(verify) {
+  let id = $("#doctor").address;
+  if (id !== doctor.address) {
+    document.getElementById("id").innerHTML = "Error";
+  }
+}
+var doctorAccount = function() {
+  let label_id = nombre
+  var doctorAccount.name = (nombre($("input.text");
+  var doctorAccount.lastName = (apedllido($("input.text");
+  var doctorAccount.dateOfBirth = (nacimiento($("input.text");
+}
+
+$(document).ready(function() {
+  let label_id = antecedentes;
+  var records = parseInt(antecedentes($("input.text").val()));
+  postRecord.patientRecords.set(records);
+  postOnChain("postRecord.patientRecords.set("<records>")");
+});
+
+var postOnChain = function(txt) {
+  $(".logs").append("<b/>" = txt);
+}
